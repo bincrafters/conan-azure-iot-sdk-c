ec8f437fca16b9067c313f2614906c9cf5a9f553
Uilian Ries <uilianries@gmail.com>
2017-09-05 20:45:42 -0300

    Skip static library build on Linux
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -1,7 +1,15 @@
 from conan.packager import ConanMultiPackager
+import platform
 
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="Azure-IoT-SDK-C:shared", pure_c=True)
+    # Skip static library
+    if platform.system() == "Linux":
+        filtered_builds = []
+        for settings, options, env_vars, build_requires in builder.builds:
+            if options["Azure-IoT-SDK-C:shared"]:
+                 filtered_builds.append([settings, options, env_vars, build_requires])
+        builder.builds = filtered_builds
     builder.run()
