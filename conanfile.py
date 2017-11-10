6af9a9aa8e1f2d81cfbb59d13ef18a0ce8439135
Uilian Ries <uilian@khomp.com>
2017-11-10 08:35:43 -0200

    Fix some points to perform build
    - After the new Azure C shared utility 1.0.46, many points
      in CMake were changed. For now, these changes won't fix
      the problem, but they help to identify where is the problem.
    - The Azure C shared utility must be updated before to continue.
      libcurl was released at bincrafters channel, providing a new and
      stable stuff, overcoming the last lasote's version.
    
    Signed-off-by: Uilian Ries <uilian@khomp.com>

--- conanfile.py
+++ conanfile.py
@@ -5,20 +5,22 @@ from conans import ConanFile, CMake, tools, util
 class AzureiotsdkcConan(ConanFile):
     name = "Azure-IoT-SDK-C"
     version = "1.1.27"
-    release_date = "2017-11-03"
+    release_date = "2017-10-20"
     generators = "cmake"
     settings = "os", "compiler", "build_type", "arch"
     url = "https://github.com/bincrafters/conan-azure-iot-sdk-c"
     description = "A C99 SDK for connecting devices to Microsoft Azure IoT services"
     license = "https://github.com/Azure/azure-iot-sdk-c/blob/master/LICENSE"
     options = {"shared": [True, False]}
-    default_options = "shared=True"
+    default_options = "shared=False"
     lib_short_name = "azure_iot_sdks"
     requires = "Azure-C-Shared-Utility/[>=1.0.46]@bincrafters/stable", \
         "Azure-uMQTT-C/[>=1.0.46]@bincrafters/stable", \
         "Azure-uAMQP-C/[>=1.0.46]@bincrafters/stable", \
-        "Parson/0.1.0@bincrafters/stable"
-        
+        "Parson/0.1.0@bincrafters/stable", \
+        # XXX (uilian): Should be solved by Azure-C-Shared-Utility
+        "libcurl/7.52.1@bincrafters/stable"
+
     def source(self):
         source_url = "https://github.com/Azure/azure-iot-sdk-c"
         tools.get("%s/archive/%s.tar.gz" % (source_url, self.release_date))
@@ -35,6 +37,7 @@ class AzureiotsdkcConan(ConanFile):
         cmake.definitions["build_as_dynamic"] = self.options.shared
         cmake.definitions["skip_samples"] = True
         cmake.definitions["use_installed_dependencies"] = True
+        # TODO (uilian): azure_c_shared_utility_DIR was removed
         cmake.definitions["azure_c_shared_utility_DIR"] = self.deps_cpp_info["Azure-C-Shared-Utility"].res_paths[0]
         cmake.definitions["uamqp_DIR"] = self.deps_cpp_info["Azure-uAMQP-C"].res_paths[0]
         cmake.definitions["umqtt_DIR"] = self.deps_cpp_info["Azure-uMQTT-C"].res_paths[0]
