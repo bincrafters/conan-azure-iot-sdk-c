a5e7b7199003df4344a715a0801e37875ab99291
Uilian Ries <uilianries@gmail.com>
2017-11-10 23:15:20 -0200

    Build on Linux
    - Solved some important point to build IoT SDK on Linux.
      All dependencies are solve by Conan, so dependencies.cmake
      is not required anymore.
    - Some linkage error was found when libcurl is used as static
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,5 @@
 import os
-from conans import ConanFile, CMake, tools, util
+from conans import ConanFile, CMake, tools
 
 
 class AzureiotsdkcConan(ConanFile):
@@ -14,41 +14,46 @@ class AzureiotsdkcConan(ConanFile):
     options = {"shared": [True, False]}
     default_options = "shared=False"
     lib_short_name = "azure_iot_sdks"
+    root_dir = "%s-%s" % (name.lower(), release_date)
     requires = "Azure-C-Shared-Utility/[>=1.0.46]@bincrafters/stable", \
         "Azure-uMQTT-C/[>=1.0.46]@bincrafters/stable", \
         "Azure-uAMQP-C/[>=1.0.46]@bincrafters/stable", \
-        "Parson/0.1.0@bincrafters/stable", \
-        # XXX (uilian): Should be solved by Azure-C-Shared-Utility
-        "libcurl/7.52.1@bincrafters/stable"
+        "Azure-uHTTP-C/[>=1.0.46]@bincrafters/stable", \
+        "Parson/0.1.0@bincrafters/stable"
 
     def source(self):
         source_url = "https://github.com/Azure/azure-iot-sdk-c"
         tools.get("%s/archive/%s.tar.gz" % (source_url, self.release_date))
-        extracted_dir = "%s-%s" % (self.name.lower(), self.release_date)
-        os.rename(extracted_dir,"sources")
+
+    def configure(self):
+        #  XXX (uilian): hidden symbol `curl_easy_getinfo' when libcurl is static
+        if self.settings.os != "Windows":
+            self.options["libcurl"].shared = True
 
     def build(self):
-        parson_dst = os.path.join("sources", "parson")
+        cmake_file = os.path.join(self.root_dir, "CMakeLists.txt")
+        conan_magic_lines = """project(azure_iot_sdks)
+        include(../conanbuildinfo.cmake)
+        CONAN_BASIC_SETUP()
+        """
+        tools.replace_in_file(cmake_file, "project(azure_iot_sdks)", conan_magic_lines)
+        tools.replace_in_file(cmake_file, 'include("dependencies.cmake")', "")
+        tools.replace_in_file(cmake_file, 'set_platform_files(${CMAKE_CURRENT_LIST_DIR}/c-utility)', "")
+        parson_dst = os.path.join(self.root_dir, "deps", "parson")
         self.copy(pattern="parson.h", dst=parson_dst, src=self.deps_cpp_info["Parson"].include_paths[0])
         self.copy(pattern="parson.c", dst=parson_dst, src=os.path.join(self.deps_cpp_info["Parson"].rootpath, "src"))
-        util.files.mkdir(os.path.join(self.deps_cpp_info["Azure-uAMQP-C"].include_paths[0], "azureiot"))
-        util.files.mkdir(os.path.join(self.deps_cpp_info["Azure-uMQTT-C"].include_paths[0], "azureiot"))
-        cmake = CMake(self)
-        cmake.definitions["build_as_dynamic"] = self.options.shared
-        cmake.definitions["skip_samples"] = True
-        cmake.definitions["use_installed_dependencies"] = True
-        # TODO (uilian): azure_c_shared_utility_DIR was removed
-        cmake.definitions["azure_c_shared_utility_DIR"] = self.deps_cpp_info["Azure-C-Shared-Utility"].res_paths[0]
-        cmake.definitions["uamqp_DIR"] = self.deps_cpp_info["Azure-uAMQP-C"].res_paths[0]
-        cmake.definitions["umqtt_DIR"] = self.deps_cpp_info["Azure-uMQTT-C"].res_paths[0]
-        cmake.definitions["AZURE_C_SHARED_UTILITY_INCLUDES"] = self.deps_cpp_info["Azure-C-Shared-Utility"].include_paths[0]
-        cmake.configure(source_dir="sources")
-        cmake.build()
+        with tools.chdir(self.root_dir):
+            cmake = CMake(self)
+            cmake.definitions["build_as_dynamic"] = self.settings.os == "Windows" and self.options.shared
+            cmake.definitions["skip_samples"] = True
+            cmake.definitions["use_installed_dependencies"] = True
+            cmake.configure(source_dir=os.getcwd())
+            cmake.build()
 
     def package(self):
         self.copy(pattern="LICENSE", dst=".", src=".")
-        self.copy(pattern="*", dst="include", src=os.path.join("sources", "iothub_client", "inc"))
-        self.copy(pattern="*", dst="include", src=os.path.join("sources", "iothub_service_client", "inc"))
+        self.copy(pattern="*", dst="include", src=os.path.join(self.root_dir, "iothub_client", "inc"))
+        self.copy(pattern="*", dst="include", src=os.path.join(self.root_dir, "iothub_service_client", "inc"))
         self.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
         self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
         self.copy(pattern="*.so*", dst="lib", src=".", keep_path=False)
@@ -56,4 +61,4 @@ class AzureiotsdkcConan(ConanFile):
         self.copy(pattern="*.dll", dst="bin", src=".", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = self.collect_libs()
+        self.cpp_info.libs = tools.collect_libs(self)
